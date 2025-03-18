397483963d94637133bebc4773ed0fc2d64602d0
Deepshika Babu <deepshika.babu@gmail.com>
2025-03-18 23:28:06 +0530

    Added folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Deepshika',
+    author_email= 'deepshika.babu@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
